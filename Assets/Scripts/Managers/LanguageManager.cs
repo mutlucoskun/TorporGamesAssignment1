8595c812b92f84673e3a90c36e8ed2f0cc0680c4
mutlucoskun <mutlucoskun@users.noreply.github.com>
2022-08-19 05:43:20 +0200

    Lang Manager (wip)

---
+++ Assets/Scripts/Managers/LanguageManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageManager : MonoBehaviour
+{
+    #region Singleton
+    private static LanguageManager _instance;
+
+    public static LanguageManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<LanguageManager>();
+            return _instance;
+        }
+    }
+    #endregion
+
+    public string Translate(string translationKey)
+    {
+        //TODO: Implement localization through translation keys
+
+        return translationKey;
+    }
+}
