a48a71492d9048912405ccfb588b2ee3eb601d58
mutlucoskun <mutlucoskun@users.noreply.github.com>
2022-08-21 12:53:46 +0200

    notes data 1st draft

---
+++ Assets/Scripts/NotesMenuData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class NotesMenuData
+{
+    public List<NotesMenuItem> NotesMenuItems = new List<NotesMenuItem>();
+}
+    
+[System.Serializable]
+public class NotesMenuItem
+{
+    public string name;
+    public string text;
+    public int sorting;
+    public List<NotesEntry> entryButtons = new List<NotesEntry>();
+}
+
+[System.Serializable]
+public class NotesEntry
+{
+    public string text;
+}
